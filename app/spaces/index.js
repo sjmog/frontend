a780d6735db48846fdaaf039bb2c6a98ebad8f5b
Sam Morgan <s_morgan@me.com>
2016-03-16 17:17:37 +0000

    scaffolded faked API calls for frontend

--- app/spaces/index.js
+++ app/spaces/index.js
@@ -9,6 +9,15 @@ angular.module('makersBnb.index', ['ngRoute'])
   });
 }])
 
-.controller('SpacesCtrl', [function() {
+.controller('SpacesCtrl', ['$scope', function($scope) {
+  // real getSpaces() function
+  // this.getSpaces = function() {
+  //   // make an HTTP request to the API for all spaces
+  // };
+  
+  this.getSpaces = function() {
+    return [{id: 1, name: "My lovely space"}, {id: 2, name: "My horrible space"}];
+  };
 
+  $scope.spaces = this.getSpaces();
 }]);
